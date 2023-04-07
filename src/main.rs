0b05299b02bdce7a059eb85b977a42d23403f1db
Maciej Ratkiewicz <matsaki95@tutanota.com>
2023-04-07 13:41:35 +0300

    Moved default parameters to own file

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,7 @@
-use crate::lfmf::LFMF_Parameters;
-
 mod cli;
+mod defaults_for_lfmf;
 mod find_max_distance;
 mod lfmf;
-
-const GROUND_PARAMETERS: LFMF_Parameters = LFMF_Parameters {
-    h_tx__meter: 10.0,
-    h_rx__meter: 10.0,
-    f__mhz: 1.0,
-    P_tx__watt: 10000.0,
-    N_s: 300.0,
-    d__km: 300.0,
-    epsilon: 22.0,
-    sigma: 0.003,
-    pol: 1,
-};
-
-const SEA_PARAMETERS: LFMF_Parameters = LFMF_Parameters {
-    h_tx__meter: 10.0,
-    h_rx__meter: 10.0,
-    f__mhz: 1.0,
-    P_tx__watt: 10000.0,
-    N_s: 300.0,
-    d__km: 300.0,
-    epsilon: 70.0,
-    sigma: 5.0,
-    pol: 1,
-};
 
 fn main() {
     let cli = cli::parse();
@@ -35,7 +10,7 @@ fn main() {
 
     let min_e = cli.min_field_strength;
     let max_search_d = cli.max_search_distance;
-    let test_parameters = GROUND_PARAMETERS;
+    let test_parameters = defaults_for_lfmf::GROUND_PARAMETERS;
 
     let result = find_max_distance::find_max_distance(min_e, test_parameters, max_search_d);
 
